b0827016efda668425d5f96e59e820e1a8382498
MasVer021 <verdino.massy@gmail.com>
2022-10-29 12:43:25 +0200

    primo merge

--- provaExec/provaSwitch.c
+++ provaExec/provaSwitch.c
@@ -25,23 +25,26 @@ int main(void)
 			}
 			exit(0);
 			break;
-		default: // padre
-			pid2 = fork(); // creazione pid 2 figlio
-			switch(pid2)
-			{
-				case -1: //errore 2 fork
-					fprintf(stderr,"errore nella creazione della fork");
-					break;
-				case 0: // figlio 2
-					printf("pid #2 figlio%d\n",getppid());
-					for(i=101;i<=150;i++)
-					{
-						printf("%d\n",i);
-					}
-					exit(0);
-					break;
-			}
-			printf("il figlio con pid %d e terminto\n",waitpid(-1,NULL,0));
-			printf("il figlio con pid %d e terminto\n",waitpid(-1,NULL,0));
 	}
+
+	pid2 = fork(); // creazione pid 2 figlio
+
+	switch(pid2)
+	{
+		case -1: //errore 2 fork
+			fprintf(stderr,"errore nella creazione della fork");
+			break;
+		case 0: // figlio 2
+			printf("pid #2 figlio%d\n",getppid());
+			for(i=101;i<=150;i++)
+			{
+				printf("%d\n",i);
+			}
+			exit(0);
+			break;
+	}
+
+	printf("il figlio con pid %d e terminto\n",waitpid(-1,NULL,0));
+	printf("il figlio con pid %d e terminto\n",waitpid(-1,NULL,0));
 }
+
